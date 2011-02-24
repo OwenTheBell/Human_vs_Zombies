cf356373c4919af7a0112582bc5b6962069adc52
icd3 <git@iandimayuga.com>
2011-02-24 01:14:27 -0500

    added collision checking to HvZWorld

--- Human_vs_Zombies/Human_vs_Zombies/HvZClasses/HvZWorld.cs
+++ Human_vs_Zombies/Human_vs_Zombies/HvZClasses/HvZWorld.cs
@@ -14,10 +14,14 @@ namespace Human_vs_Zombies.HvZClasses
     {
         private Player m_Player;
         private SortedDictionary<ulong, Entity> m_Entities;
+        private SortedDictionary<ulong, List<Entity>> m_ColMatrix;
+        private bool m_ColUpdated;
 
         public HvZWorld()
         {
             m_Player = new Player(this, new Vector2(100f, 100f), 0f, 0f, Vector2.Zero);
+            this.m_Entities = new SortedDictionary<ulong, Entity>();
+            this.m_ColMatrix = null;
         }
 
         public Player GetPlayer()
@@ -25,9 +29,86 @@ namespace Human_vs_Zombies.HvZClasses
             return m_Player;
         }
 
+        private void CheckCols()
+        {
+            m_ColMatrix = new SortedDictionary<ulong, List<Entity>>();
+            for (int i = 0; i < m_Entities.Values.Count - 1; i++ )
+            {
+                for (int j = i + 1; j < m_Entities.Values.Count; j++)
+                {
+                    Entity one = m_Entities.Values.ElementAt(i);
+                    Entity two = m_Entities.Values.ElementAt(j);
+
+                    if ((one.GetPosition() - two.GetPosition()).LengthSquared() <= Math.Pow((one.GetRadius() + two.GetRadius()), 2))
+                    {
+                        if (m_ColMatrix.ContainsKey(one.GetID()))
+                        {
+                            List<Entity> cols = new List<Entity>();
+                            cols.Add(two);
+                            m_ColMatrix.Add(one.GetID(), cols);
+                        }
+                        else
+                        {
+                            List<Entity> cols;
+                            m_ColMatrix.TryGetValue(one.GetID(), out cols);
+                            cols.Add(two);
+                        }
+
+                        if (m_ColMatrix.ContainsKey(two.GetID()))
+                        {
+                            List<Entity> cols = new List<Entity>();
+                            cols.Add(one);
+                            m_ColMatrix.Add(two.GetID(), cols);
+                        }
+                        else
+                        {
+                            List<Entity> cols;
+                            m_ColMatrix.TryGetValue(two.GetID(), out cols);
+                            cols.Add(one);
+                        }
+                    }
+                }
+            }
+
+            m_ColUpdated = true;
+        }
+
+        public List<Entity> Collisions(Entity entity)
+        {
+            if (!m_ColUpdated)
+            {
+                return new List<Entity>();
+            }
+            else
+            {
+                List<Entity> ret = new List<Entity>();
+                m_ColMatrix.TryGetValue(entity.GetID(), out ret);
+                return ret;
+            }
+        }
+
+        private void KillDeadEntities()
+        {
+            m_ColUpdated = false;
+            foreach(Entity e in m_Entities.Values)
+            {
+                if (e.GetDead())
+                {
+                    m_Entities.Remove(e.GetID());
+                }
+            }
+        }
+
         public void Update(float dTime)
         {
+            this.CheckCols();
 
+            foreach (Entity e in m_Entities.Values)
+            {
+                e.Update(dTime);
+            }
+
+            this.KillDeadEntities();
         }
 
         public void addEntity(Entity entity)
@@ -47,7 +128,10 @@ namespace Human_vs_Zombies.HvZClasses
                 SpriteEffects.None,
                 0f);
 
-            this.m_Player.Draw();
+            foreach (Entity e in m_Entities.Values)
+            {
+                e.Draw();
+            }
         }
     }
 }
