27574fd8d71d81c267af49cce23512b9045ff5e1
Sean Kruer <greenybeard@gmail.com>
2011-02-24 05:30:06 -0500

    Stop acceleration
    That shit was whack

--- Human_vs_Zombies/Human_vs_Zombies/Controls/HumanBrains.cs
+++ Human_vs_Zombies/Human_vs_Zombies/Controls/HumanBrains.cs
@@ -31,8 +31,8 @@ namespace Human_vs_Zombies.Controls
         public override void update(float dTime, Vector2 position)
         {
             this.m_Walk = 
-                new Vector2(position.X - 10 * GameWorld.controller.ContainsFloat(ActionType.MoveHorizontal), 
-                position.Y - 10 * GameWorld.controller.ContainsFloat(ActionType.MoveVertical));
+                new Vector2(10 * GameWorld.controller.ContainsFloat(ActionType.MoveHorizontal), 
+                10 * GameWorld.controller.ContainsFloat(ActionType.MoveVertical));
             this.m_Shoot = new Vector2(GameWorld.controller.ContainsFloat(ActionType.LookVertical), GameWorld.controller.ContainsFloat(ActionType.LookHorizontal));
             float length = (float) Math.Sqrt(Math.Pow(this.m_Shoot.X, 2) + Math.Pow(this.m_Shoot.Y, 2));
             this.m_Shoot.X = this.m_Shoot.X / length;
