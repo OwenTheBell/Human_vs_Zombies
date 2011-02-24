17bcc87fd0d723747fc87ef4015387b1388cb157
icd3 <git@iandimayuga.com>
2011-02-24 01:53:06 -0500

    Projectiles die at the right times and also upon collision

--- Human_vs_Zombies/Human_vs_Zombies/HvZClasses/Mobs/Projectile.cs
+++ Human_vs_Zombies/Human_vs_Zombies/HvZClasses/Mobs/Projectile.cs
@@ -8,10 +8,27 @@ namespace Human_vs_Zombies.HvZClasses.Mobs
 {
     public class Projectile : Mob
     {
+        public static const float LIFE = 0.5f;
+
+        private float m_Life;
+
         public Projectile(HvZWorld hvzWorld, Vector2 position, Vector2 rotation, float radius, Vector2 velocity)
             : base(hvzWorld, position, rotation, radius, velocity, 10f)
         {
+            m_Life = LIFE;
         }
+
+        public override void Update(float dTime)
+        {
+            if (m_Life < 0 || this.GetWorld().Collisions(this).Count > 0)
+            {
+                this.SetDead(true);
+            }
+
+            m_Life -= dTime;
+            base.Update(dTime);
+        }
+
         public override void Draw() { }
     }
 }
