34e860371550c09cde346db7d8c043afaeda0b6e
Owen Bell <oab7@case.edu>
2011-03-03 13:50:15 -0500

    There are now credits in the start screen, pretty rough though

--- Human_vs_Zombies/Human_vs_Zombies/Screens/StartScreen.cs
+++ Human_vs_Zombies/Human_vs_Zombies/Screens/StartScreen.cs
@@ -32,6 +32,10 @@ namespace Human_vs_Zombies.Screens
         /// Where to write "Paused"
         /// </summary>
         private Vector2 textDrawPosition;
+        private Vector2 textDrawPosition2;
+        private Vector2 textDrawPosition3;
+        private Vector2 textDrawPosition4;
+        private Vector2 textDrawPosition5;
 
         /// <summary>
         /// The center of the word "Paused".
@@ -47,14 +51,18 @@ namespace Human_vs_Zombies.Screens
             // Note: Do not use GameClock, it will be paused!
             this.initialTime = DateTime.Now.Ticks;
             this.menu = new StartMenu(
-                new Vector2(Settings.worldWidth / 2, 1000 * Drawer.GetRatio()),
+                new Vector2(Settings.worldWidth / 2 + 500, 1000 * Drawer.GetRatio()),
                 new MenuAction[]
                 { 
                     new MenuAction(ActionType.GoBack, new StartGameDelegate()),
                 },
                 75f);
 
-            this.textDrawPosition = new Vector2(Settings.worldWidth / 2, 700*Drawer.GetRatio());
+            this.textDrawPosition = new Vector2(Settings.worldWidth / 2, 700 * Drawer.GetRatio());
+            this.textDrawPosition2 = new Vector2(Settings.worldWidth / 2, 700 * Drawer.GetRatio() + 200);
+            this.textDrawPosition3 = new Vector2(Settings.worldWidth / 2, 700 * Drawer.GetRatio() + 300);
+            this.textDrawPosition4 = new Vector2(Settings.worldWidth / 2, 700 * Drawer.GetRatio() + 400);
+            this.textDrawPosition5 = new Vector2(Settings.worldWidth / 2, 700 * Drawer.GetRatio() + 500);
             this.textDrawOrigin = Drawer.font.MeasureString("Human Vs. Zombies") / 2f;
         }
 
@@ -92,6 +100,46 @@ namespace Human_vs_Zombies.Screens
                 0f,
                 this.textDrawOrigin,
                 4f,
+                SpriteEffects.None,
+                1f);
+
+            Drawer.DrawString(
+                "Ian Dimayuga",
+                this.textDrawPosition2,
+                Color.Red,
+                0f,
+                this.textDrawOrigin,
+                2f,
+                SpriteEffects.None,
+                1f);
+
+            Drawer.DrawString(
+                "Owen Bell",
+                this.textDrawPosition3,
+                Color.Red,
+                0f,
+                this.textDrawOrigin,
+                2f,
+                SpriteEffects.None,
+                1f);
+
+            Drawer.DrawString(
+                "Sean Kruer",
+                this.textDrawPosition4,
+                Color.Red,
+                0f,
+                this.textDrawOrigin,
+                2f,
+                SpriteEffects.None,
+                1f);
+
+            Drawer.DrawString(
+                "Tom Dooner",
+                this.textDrawPosition5,
+                Color.Red,
+                0f,
+                this.textDrawOrigin,
+                2f,
                 SpriteEffects.None,
                 1f);
 
