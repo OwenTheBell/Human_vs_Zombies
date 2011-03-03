327dd35dab4a7fe84d8d5c1bed2e3a49d3384f38
Tom Dooner <tomdooner@gmail.com>
2011-03-03 02:53:55 -0500

    Adds the stuff necessary for the Nukes to work. Derp.

---
+++ Human_vs_Zombies/Human_vs_Zombies/HvZClasses/Items/ExplosionItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Human_vs_Zombies.Rendering;
+using Microsoft.Xna.Framework.Graphics;
+using Human_vs_Zombies.HvZClasses.Items;
+using Human_vs_Zombies.HvZClasses.Mobs;
+
+namespace Human_vs_Zombies.HvZClasses.Items
+{
+    public class ExplosionItem : Item
+    {
+        private HvZWorld world;
+
+        public ExplosionItem(HvZWorld hvzWorld, Vector2 position, Vector2 rotation, float radius, float lifespan)
+            : base(hvzWorld, position, rotation, radius, lifespan)
+        {
+            this.m_Texture = TextureStatic.Get("Nuke");
+            world = hvzWorld;
+        }
+
+        public override void OnPickup(Player player)
+        {
+            this.SetDead(true);
+            world.KillAllZombies();
+           
+        }
+    }
+}
